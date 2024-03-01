3088b32b70884105f16ecbb0ba1c84fccbcb5ec3
Raja Venkata Shandilya Puram <103808761+RVSHANDILYA@users.noreply.github.com>
2024-03-01 16:19:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import gradio as gr
 def hello(name):
     return "Hello " + name
 st.title("An application deployment using Streamlit ")
 name = st.text_input("Enter your name")
 result = hello(name)
 st.write("-->", result)
-gr.Interface(fn=hello, inputs='text', outputs='text').launch()
+
